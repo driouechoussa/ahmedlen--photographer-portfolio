c4bb397bc2a7e4e62fbb13f79a971ebf53e3ca07
Ousssama Driouech <thedriwsh@gmail.com>
2025-04-13 20:57:48 +0100

    add some sections with styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,3 @@
- // Wait until page fully loads
- window.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("heroTitle").classList.add("fade-in");
-    document.getElementById("heroTagline").classList.add("fade-in");
-});
-
-const revealOnScroll = () => {
-    const reveals = document.querySelectorAll('.about-img, .about-name, .about-desc, .service-box');
-    const windowHeight = window.innerHeight;
-
-    reveals.forEach((el) => {
-        const elementTop = el.getBoundingClientRect().top;
-        if (elementTop < windowHeight - 100) {
-            el.classList.add('reveal');
-        }
-    });
-};
-
-window.addEventListener("load", function () {
-    const loader = document.getElementById("loader-wrapper");
-    loader.style.opacity = "0";
-    setTimeout(() => {
-        loader.style.display = "none";
-    }, 600);
-});
 
 const reviewCarousel = document.getElementById('reviewCarousel');
 const carousel = new bootstrap.Carousel(reviewCarousel, {
@@ -31,5 +6,3 @@ const carousel = new bootstrap.Carousel(reviewCarousel, {
     pause: false
 });
 
-window.addEventListener('scroll', revealOnScroll);
-window.addEventListener('load', revealOnScroll); 
